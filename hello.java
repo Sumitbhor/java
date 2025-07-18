78f9135e0ad2d6e48e26fb3952010b992ae48bbb
Sumit Sudam Bhor <sumitbhor227@gmail.com>
2025-07-18 12:57:44 +0530

    command line arguement

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-    }
+        int a =Integer.parseInt(args[0]);
+        int b =Integer.parseInt(args[1]);
+        System.out.println(a+b);
+       // System.out.println(b);
+    
+    }   
 }
